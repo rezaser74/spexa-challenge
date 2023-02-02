727adbb61f2d39e48be098af56173eaabea54c5b
shemsuho95 <rezaser3774@gmail.com>
2023-02-02 17:58:26 +0330

    Replace IndexPage.vue as login page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,15 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="full-width  flex column items-center q-col-gutter">
+      <span class="text-weight-bold text-center q-ma-lg">try the product out for free</span>
+      <q-form class="flex column items-center justify-center form q-col-gutter-y" @click.prevent="loginUser">
+        <q-input dense class="full-width" outlined v-model="data.email" type="email" placeholder="email"
+                 :rules="[val => !!val || 'Email is missing']"/>
+        <q-input dense class="full-width" outlined type="password" v-model="data.password" placeholder="password"
+                 :rules="[val=>!!val||'Password is missing']"/>
+        <q-btn type="submit" color="black" align="center" class="full-width">Login/Register</q-btn>
+      </q-form>
+    </div>
   </q-page>
 </template>
 
@@ -12,6 +17,24 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      data: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    loginUser () {
+      this.$router.push('/home')
+    }
+  }
 })
 </script>
+<style>
+.form {
+  min-width: 20vw;
+}
+</style>

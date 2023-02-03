b3d505cbe8e776ef7c166862ac99ed14b3d308d0
shemsuho95 <rezaser3774@gmail.com>
2023-02-03 21:41:51 +0330

    set create directory api

--- src/components/CreateDirectory.vue
+++ src/components/CreateDirectory.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-page class="flex  justify-center items-center">
+  <q-page class="flex column justify-center items-center">
+    <bread-crumb/>
     <div class="flex flex-center column  items-center box">
       <q-icon name="create_new_folder" color="black" size="10em"></q-icon>
       <span class="">there is nothing to show</span>
@@ -7,25 +8,26 @@
     </div>
   </q-page>
   <div>
-    <q-dialog v-model="isOpen">
-      <div class="flex column justify-center bg-white q-px-xl q-py-sm q-col-gutter-y-md">
-        <span class="text-h6 self-start">Create Directory</span>
-        <q-input v-model="name" placeholder="title"/>
-        <div class="row q-gutter-sm">
-          <q-btn color="black" outline v-close-popup dense>cancel</q-btn>
-          <q-btn class="q-px-sm" color="black" dense>Create</q-btn>
-        </div>
-      </div>
-    </q-dialog>
+    <create-modal :show="isOpen"/>
   </div>
 </template>
 
 <script>
+import CreateModal from 'components/CreateModal.vue'
+import BreadCrumb from 'components/BreadCrumb.vue'
+
 export default {
   name: 'CreateDirectory',
+  components: { BreadCrumb, CreateModal },
   data () {
     return {
-      isOpen: false
+      isOpen: false,
+      title: ''
+    }
+  },
+  methods: {
+    create () {
+      console.log('created')
     }
   }
 }

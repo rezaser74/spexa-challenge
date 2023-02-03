d5d63b0d9b027a8ae3b4f84f5aa99d21295fe021
shemsuho95 <rezaser3774@gmail.com>
2023-02-03 21:40:42 +0330

    add BreadCrumb.vue

--- src/components/BreadCrumb.vue
+++ src/components/BreadCrumb.vue
@@ -1,14 +1,37 @@
 <template>
-  <q-breadcrumbs>
-    <q-breadcrumbs-el label="Home" />
-    <q-breadcrumbs-el label="Components" />
-    <q-breadcrumbs-el label="Breadcrumbs" />
-  </q-breadcrumbs>
+  <div class="q-mb-lg">
+    <q-breadcrumbs>
+      <q-breadcrumbs-el class="cursor-pointer" v-for="(directory,index) in directories"
+                        :icon="index===0?'home' : 'folder'" :key="index"
+                        @click.prevent="goToDirectory(directory)">
+        {{ directory.title }}
+      </q-breadcrumbs-el>
+
+    </q-breadcrumbs>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'BreadCrumb'
+  name: 'BreadCrumb',
+  computed: {
+    ...mapGetters({
+      directories: 'directories/getDirList'
+    })
+  },
+  methods: {
+    async goToDirectory (item) {
+      const array = this.directories.map(a => a)
+      const Index = array.indexOf(item)
+      const target = array.splice(0, (Index + 1))
+      console.log('target', target)
+      await this.$router.push(`/directories/${item.title}`)
+      await this.$store.dispatch('directories/getDirectory', item.id)
+      await this.$store.commit('directories/setBC', target)
+    }
+  }
 }
 </script>
 

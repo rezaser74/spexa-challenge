aee746b10c6476d50f7eb953815b5fada7cf589d
shemsuho95 <rezaser3774@gmail.com>
2023-02-03 21:41:24 +0330

    add Api to delete modal

--- src/components/DeleteModal.vue
+++ src/components/DeleteModal.vue
@@ -6,7 +6,7 @@
       <span>do you want to delete this direcotry?</span>
       <div class="row q-gutter-sm full-width">
         <q-btn class="btn" color="black" outline v-close-popup dense>cancel</q-btn>
-        <q-btn class="q-px-sm btn" color="black" dense>Delete</q-btn>
+        <q-btn class="q-px-sm btn" color="black" dense @click.prevent="deleteIt">Delete</q-btn>
       </div>
     </div>
   </q-dialog>
@@ -14,10 +14,13 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   name: 'DeleteModal',
   props: {
-    show: Boolean
+    show: Boolean,
+    id: String,
+    title: String
   },
   data () {
     return {
@@ -27,6 +30,17 @@ export default {
   watch: {
     'show' () {
       this.isOpen = !this.isOpen
+    }
+  },
+  methods: {
+    ...mapActions({
+      deleteItem: 'directories/deleteDirectory'
+    }),
+    async deleteIt () {
+      const id = this.id
+      const title = this.title
+      console.log(this.title, this.id)
+      await this.deleteItem({ title, id })
     }
   }
 }
